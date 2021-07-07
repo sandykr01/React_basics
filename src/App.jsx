9296a9bd061a5e191dd302f14a4fae5730b10830
sk9873422561 <sk9873422561@gmail.com>
2021-07-07 22:29:10 +0530

    added component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sandeep from "./components/user";
+
+// JSX
+function App() {
+  return <div>
+    <h1>Sandeep</h1>
+    <Sandeep name="Sandy" description="FSWD" />
+  </div>;
+}
+
+export default App;
---
+++ src/components/user.jsx
@@ -0,0 +1,13 @@
+const User = (props) => {
+    return (
+        <div>
+            <h1>
+                {props.name}
+            </h1>
+            <p>{props.description}</p>
+        </div>
+    );
+};
+
+
+export default User;
